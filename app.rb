72c61591e2141687861558482206f9d9a3b87653
Thierry Chau <thierry.chau@gmail.com>
2023-07-24 23:35:30 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
 require "sinatra"
 require "sinatra/reloader"
 
+rbs = ["rock", "paper", "scissors"]
+
+def rbs_game(user_rbs, computer_rbs)
+  if user_rbs == computer_rbs
+    return "tied"
+  elsif (user_rbs == "rock" && computer_rbs == "scissors") || (user_rbs == "scissors" && computer_rbs == "paper") || (user_rbs == "paper" && computer_rbs == "rock")
+    return "won"
+  else
+    return "lost"
+  end
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  @player_rbs = "rock"
+
+  @computer_rbs = rbs.sample
+
+  @rbs_outcome = rbs_game(@player_rbs, @computer_rbs)
+
+  erb(:rock)
+end
+
+get("/paper") do
+  @player_rbs = "paper"
+
+  @computer_rbs = rbs.sample
+
+  @rbs_outcome = rbs_game(@player_rbs, @computer_rbs)
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  @player_rbs = "scissors"
+
+  @computer_rbs = rbs.sample
+
+  @rbs_outcome = rbs_game(@player_rbs, @computer_rbs)
+
+  erb(:scissors)
 end
